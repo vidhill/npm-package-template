f0ea3ee2e1c18be26b1f11a4c460a93582839e88
David Hill <dh366k@us.att.com>
2021-06-20 21:23:48 +0100

    Make eslint config extend off of eslint-config-vidhill

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,31 +3,10 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    plugins: ['prettier', 'import', 'unicorn'],
-    extends: 'eslint:recommended',
+    extends: ['vidhill'],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: 'module',
     },
-    rules: {
-        'prettier/prettier': 'error',
-        'no-console': 'error',
-        'prefer-const': 'error',
-        'import/no-unresolved': 'error',
-        'no-useless-rename': 'error',
-        'unicorn/no-abusive-eslint-disable': 'error',
-        'unicorn/explicit-length-check': 'error',
-    },
     ignorePatterns: ['build/**/*'],
-    overrides: [
-        {
-            // unit test overrides
-            files: ['jest-config/*', '*.spec.js'],
-            plugins: ['jest'],
-            extends: ['plugin:jest/recommended'],
-            env: {
-                jest: true,
-            },
-        },
-    ],
 };
